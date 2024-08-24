802b63a644357ea4289ed81b2df341aee23352ba
Alexis Drai <alexis@alexisdrai.com>
2024-08-24 23:26:57 +0200

    Get ready to rumble

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
+// routes/api.js
+
 'use strict';
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+  // TODO Complete the necessary routes
   let convertHandler = new ConvertHandler();
 
 };
